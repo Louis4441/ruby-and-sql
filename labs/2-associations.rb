f165bef8fa69ccc7dfa07d981dfc494d5e90fb6d
Louis <louis.harel@kellogg.northwestern.edu>
2022-02-01 17:05:20 +0000

    completed lab 2

--- labs/2-associations.rb
+++ labs/2-associations.rb
@@ -19,7 +19,28 @@ for contact in Contact.all
     puts "#{contact.first_name} #{contact.last_name} - #{contact.email} - #{contact.company.name}"
 end
 
-# 2. similar to above, but this time organized by company, write code to display each company (name) and its contacts, e.g.:
+# 2. similar to above, but this time organized by company, write code to display each company (name) and its contacts, e.g. below
+
+apple = Company.where( {name: "Apple Inc." })[0]
+puts "--"
+puts "#{apple.name}"
+for contact in apple.contacts
+    puts "#{contact.first_name} #{contact.last_name} - #{contact.email} "
+end
+
+amazon = Company.where( {name: "Amazon.com, Inc." })[0]
+puts "--"
+puts "#{amazon.name}"
+for contact in amazon.contacts
+    puts "#{contact.first_name} #{contact.last_name} - #{contact.email} "
+end
+
+tesla = Company.where( {name: "Tesla Inc." })[0]
+puts "--"
+puts "#{tesla.name}"
+for contact in tesla.contacts
+    puts "#{contact.first_name} #{contact.last_name} - #{contact.email} "
+end
 
 # ---------------------------------
 # Apple Inc.
@@ -31,14 +52,3 @@ end
 #
 # Tesla, Inc.
 # Elon Musk - elon@tesla.com
-
-puts "Contacts: #{Contact.all.count}"
-
-apple = Company.where( {name: "Apple Inc." })[0]
-amazon = Company.where( {name: "Apple Inc." })[0]
-tesla = Company.where( {name: "Apple Inc." })[0]
-
-
-for contact in Contact.all
-    puts "#{contact.first_name} #{contact.last_name} - #{contact.email} - #{contact.company.name}"
-end
