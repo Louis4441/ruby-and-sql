ea9fadb5e8b8d1c0d2fae5765a01919741f5958d
Louis <louis.harel@kellogg.northwestern.edu>
2022-02-01 16:13:26 +0000

    added company_id

--- labs/1-models.rb
+++ labs/1-models.rb
@@ -12,19 +12,29 @@ Contact.destroy_all
 # 1b. check out the model file
 
 # 2. create 1-2 new contacts for each company (they can be made up)
+apple = Company.where({ name: "Apple Inc." })[0]
+apple_id = apple.id
+
 values = { first_name: "John",
             last_name: "Smith",
             email: "john.smith@email.com",
-            phone_number: "8883335065" }
+            phone_number: "8883335065",
+            company_id: apple_id }
 contact1 = Contact.new(values)
 contact1.save
 
-values = { first_name: "Jane",
-            last_name: "Doe",
-            email: "jane.doe@email.com",
-            phone_number: "9995553333" }
-contact2 = Contact.new(values)
+amazon = Company.where({ name: "Amazon.com, Inc." })[0]
+amazon_id = amazon.id
+
+contact2 = Contact.new
+contact2.first_name = "Jane"
+contact2.last_name = "Doe"
+contact2.email = "jane.doe@email.com"
+contact2.phone_number = "9995553333" 
+#contact2 = Contact.new(values)
+contact2.company_id = amazon_id
 contact2.save
+
 
 # 3. write code to display how many contacts are in the database AND each contact's info (name, email), e.g.:
 
@@ -37,6 +47,8 @@ contact2.save
 
 puts "Contacts: #{Contact.all.count}"
 for contact in Contact.all
-    puts "#{contact.read_attribute(:first_name)} #{contact.read_attribute(:last_name)} - #{contact.read_attribute(:email)}"
+    print "#{contact.read_attribute(:first_name)} #{contact.read_attribute(:last_name)} - #{contact.read_attribute(:email)}"
+    company = Company.where({ id: contact.company_id })[0]
+    print " -- Company: #{company.name}\n"
 end
 
